65afce6a83dec4156fd56b181bd4e3ba22a44047
matam basavaraju <57475017+basavaraju496@users.noreply.github.com>
2022-10-27 21:53:16 +0530

    Update counter_mod10.v

--- SV/counter_mod10.v
+++ SV/counter_mod10.v
@@ -3,21 +3,17 @@ module counter_mod10(input clk,reset,enable,output reg [3:0]counter_out);
 
 always@(posedge clk)
 		begin//{
-			if(reset==0)
+			if(reset==0 || counter_out==10)
 					begin
-						counter_out=0;
+						counter_out<=0;
 					end
 			else if(enable==0)
 				begin
-					if(counter_out<10)
-						counter_out=counter_out+1;
-					else 
-					counter_out=0;
-					
+				    counter_out<=counter_out+1;	
 				end
 			else
 				begin
-					counter_out=counter_out;
+					counter_out<=counter_out;
 				end
 
 		end//}
