b82fb3250198da8c53cbc7483e4ed5a986a24ba7
matam basavaraju <57475017+basavaraju496@users.noreply.github.com>
2022-11-10 08:24:12 +0530

    Add files via upload

---
+++ Sequential_Circuits/project_1_up_down_counter/tb_tester.v
@@ -0,0 +1,495 @@
+`include "tester.v"
+
+module tb_tester;
+reg clk;
+reg reset;
+reg ncs,nrd,nwr,start,a0,a1;
+wire [7:0]wire_din;   // 
+wire err,ec,dir;
+reg [7:0]reg_din;         
+wire [7:0] count;
+
+assign wire_din =(nwr==0 && ncs==0 )?reg_din:8'bz;  // register values given to wire din
+
+up_down_counter255_tester DUT(wire_din,clk,ncs,nrd,nwr,start,reset,a0,a1,count,err,ec,dir);
+
+
+
+// ======= Initial Conditions =======================//
+initial
+	 begin
+ 		clk = 1'b0;
+	    	//	$dumpfile("dump.vcd"); $dumpvars;    // for EDA
+		end
+
+initial
+	 begin
+		 main;  // calling main task at zero simulation time
+	 end
+
+task main;
+	 fork
+		 clock_gen;
+		// operation_flow;
+        now_testing;
+		 debug_output;
+		 endsimulation;
+ 	join
+endtask
+//====================clock generation====================//
+task clock_gen;
+ 		begin
+			 forever #1 clk = ~clk; // clock period = 2ns
+		 end
+endtask
+//===================== Generating Test Vectors ==========//
+
+task now_testing;
+begin
+
+//@(negedge clk) ncs=1; reset=1;  nwr=0; {a0,a1}=1; nrd=
+
+ncs=0; reset=1; nwr=0;   nrd=1;
+@(negedge clk) a0=0; a1=0; reg_din=10; // plr
+@(negedge clk) a0=1; a1=0; reg_din=15;    // ulr
+@(negedge clk) a0=0; a1=1; reg_din=5;    // llr
+@(negedge clk) a0=1; a1=1; reg_din=2;    //ccr    data loaded   reset data after writing
+
+
+$display(" start pulse applied ");
+@(negedge clk) nwr=1;
+@(negedge clk)  start=1;                // start pulse given at negedge of clock
+@(negedge clk )  start=0;                  // start pulse duration is one clock cycle
+#10
+@(negedge clk)   ncs=1;
+end
+    endtask
+
+
+
+
+
+task operation_flow;
+ begin//{
+
+   //=============RESET OPERATION ========================//
+
+//======================= #0  ===========================//
+@(negedge clk) ncs=0; reset=0;     /// + anything reg_din=150; {a0,a1}=1;  nwr=0; nrd=0;
+
+
+
+$display(" start pulse applied ");
+
+@(negedge clk)  start=1;                // start pulse given at negedge of clock
+@(negedge clk )  start=0;                  // start pulse duration is one clock cycle
+
+//======================== #1 ==============================//
+@(negedge clk) ncs=0; reset=1; 
+
+$display(" start pulse applied ");
+
+@(negedge clk)  start=1;                // start pulse given at negedge of clock
+@(negedge clk )  start=0;                  // start pulse duration is one clock cycle
+
+
+//======================= #2 ==============================//
+
+
+
+@(negedge clk) ncs=1; reset=0; 
+
+$display(" start pulse applied ");
+
+@(negedge clk)  start=1;                // start pulse given at negedge of clock
+@(negedge clk )  start=0;                  // start pulse duration is one clock cycle
+
+//======================= #3 ==============================//
+
+@(negedge clk) ncs=1; reset=1; 
+$display(" start pulse applied ");
+
+@(negedge clk)  start=1;                // start pulse given at negedge of clock
+@(negedge clk )  start=0;                  // start pulse duration is one clock cycle
+
+
+
+//======================= #123.1 ==============================//
+//========== #5.1 basic read operation before writing =======================//
+
+$display("basic read operation");
+
+@(negedge clk) a0=0; a1=0; nrd=0; nwr=1;  // plr reading
+@(negedge clk) a0=0; a1=1;    // reading ulr
+@(negedge clk) a0=1; a1=1;    // reading llr
+@(negedge clk) a0=1; a1=0;    // reading clr
+
+
+
+//============= #5 basic write operation=======================//
+
+$display("basic write operation");
+
+ncs=0; reset=1; nwr=0;   nrd=1;
+@(negedge clk) a0=0; a1=0; reg_din=10; // plr
+@(negedge clk) a0=1; a1=0; reg_din=15;    // ulr
+@(negedge clk) a0=0; a1=1; reg_din=5;    // llr
+@(negedge clk) a0=1; a1=1; reg_din=2;    //ccr    data loaded   reset data after writing
+
+
+//========== #5.1 basic read operation after writing =======================//
+
+$display("basic read operation");
+
+@(negedge clk) a0=0; a1=0; ncs=0; reset=1; nrd=0; nwr=1;  // plr reading
+@(negedge clk) a0=0; a1=1;    // reading ulr
+@(negedge clk) a0=1; a1=1;    // reading llr
+@(negedge clk) a0=1; a1=0;    // reading clr
+
+
+//==================== #5.2 basic start ====================//
+//================== # basic count starts here====================//
+
+$display(" start pulse applied ");
+
+@(negedge clk) nwr=1; start=1;   nrd=1;             // start pulse given at negedge of clock
+@(negedge clk )  start=0;                  // start pulse duration is one clock cycle
+
+
+//====================count ends ==============================//
+
+
+
+
+//============== DATA combinations ===========================//
+
+//~~~~~~~~~~~~~~~~~ #1 PLR=ULR=LLR ~~~~~~~~~~~~~~~~~~~~~~~~//
+
+
+nwr=0; reset=1; ncs=0; nrd=1;
+@(negedge clk) a0=0; a1=0; reg_din=5; // plr
+@(negedge clk) a0=1; a1=0; reg_din=5;    // ulr
+@(negedge clk) a0=0; a1=1; reg_din=5;    // llr
+@(negedge clk) a0=1; a1=1; reg_din=2;    //ccr reg_din=0;  reg_din=3; reg_din=5;  CHANGE CCR
+
+$display(" start pulse applied ");
+
+@(negedge clk) nwr=1; start=1;   nrd=1;             // start pulse given at negedge of clock
+@(negedge clk )  start=0;                  // start pulse duration is one clock cycle
+
+
+//~~~~~~~~~~~~~~~~~ #2 PLR=ULR > LLR  ~~~~~~~~~~~~~~~~~~~~~~~~//
+
+
+nwr=0; reset=1; ncs=0; nrd=1;
+@(negedge clk) a0=0; a1=0; reg_din=5; // plr
+@(negedge clk) a0=1; a1=0; reg_din=5;    // ulr
+@(negedge clk) a0=0; a1=1; reg_din=2;    // llr
+@(negedge clk) a0=1; a1=1; reg_din=2;    //ccr reg_din=0;  reg_din=3; reg_din=5;  CHANGE CCR
+
+$display(" start pulse applied ");
+
+@(negedge clk) nwr=1; start=1;   nrd=1;             // start pulse given at negedge of clock
+@(negedge clk )  start=0;                  // start pulse duration is one clock cycle
+
+
+//~~~~~~~~~~~~~~~~~ #3 PLR=ULR < LLR  ~~~~~~~~~~~~~~~~~~~~~~~~//
+
+
+nwr=0; reset=1; ncs=0; nrd=1;
+@(negedge clk) a0=0; a1=0; reg_din=5; // plr
+@(negedge clk) a0=1; a1=0; reg_din=5;    // ulr
+@(negedge clk) a0=0; a1=1; reg_din=7;    // llr
+@(negedge clk) a0=1; a1=1; reg_din=2;    //ccr reg_din=0;  reg_din=3; reg_din=5;  CHANGE CCR
+
+$display(" start pulse applied ");
+
+@(negedge clk) nwr=1; start=1;   nrd=1;             // start pulse given at negedge of clock
+@(negedge clk )  start=0;                  // start pulse duration is one clock cycle
+
+
+
+//~~~~~~~~~~~~~~~~~ #4 PLR=LLR > ULR ~~~~~~~~~~~~~~~~~~~~~~~~//
+
+
+nwr=0; reset=1; ncs=0; nrd=1;
+@(negedge clk) a0=0; a1=0; reg_din=5; // plr
+@(negedge clk) a0=1; a1=0; reg_din=2;    // ulr
+@(negedge clk) a0=0; a1=1; reg_din=5;    // llr
+@(negedge clk) a0=1; a1=1; reg_din=2;    //ccr reg_din=0;  reg_din=3; reg_din=5;  CHANGE CCR
+
+$display(" start pulse applied ");
+
+@(negedge clk) nwr=1; start=1;   nrd=1;             // start pulse given at negedge of clock
+@(negedge clk )  start=0;                  // start pulse duration is one clock cycle
+
+
+
+//~~~~~~~~~~~~~~~~~ #5 PLR=LLR < ULR ~~~~~~~~~~~~~~~~~~~~~~~~//
+
+
+nwr=0; reset=1; ncs=0; nrd=1;
+@(negedge clk) a0=0; a1=0; reg_din=5; // plr
+@(negedge clk) a0=1; a1=0; reg_din=10;    // ulr
+@(negedge clk) a0=0; a1=1; reg_din=5;    // llr
+@(negedge clk) a0=1; a1=1; reg_din=2;    //ccr reg_din=0;  reg_din=3; reg_din=5;  CHANGE CCR
+$display(" start pulse applied ");
+
+@(negedge clk) nwr=1; start=1;   nrd=1;             // start pulse given at negedge of clock
+@(negedge clk )  start=0;                  // start pulse duration is one clock cycle
+
+
+
+//~~~~~~~~~~~~~~~~~ #6 ULR=LLR > PLR ~~~~~~~~~~~~~~~~~~~~~~~~//
+
+
+nwr=0; reset=1; ncs=0; nrd=1;
+@(negedge clk) a0=0; a1=0; reg_din=2; // plr
+@(negedge clk) a0=1; a1=0; reg_din=5;    // ulr
+@(negedge clk) a0=0; a1=1; reg_din=5;    // llr
+@(negedge clk) a0=1; a1=1; reg_din=2;    //ccr reg_din=0;  reg_din=3; reg_din=5;  CHANGE CCR
+
+$display(" start pulse applied ");
+
+@(negedge clk) nwr=1; start=1;   nrd=1;             // start pulse given at negedge of clock
+@(negedge clk )  start=0;                  // start pulse duration is one clock cycle
+
+
+//~~~~~~~~~~~~~~~~~ #7 ULR=LLR <PLR ~~~~~~~~~~~~~~~~~~~~~~~~//
+
+
+nwr=0; reset=1; ncs=0; nrd=1;
+@(negedge clk) a0=0; a1=0; reg_din=10; // plr
+@(negedge clk) a0=1; a1=0; reg_din=5;    // ulr
+@(negedge clk) a0=0; a1=1; reg_din=5;    // llr
+@(negedge clk) a0=1; a1=1; reg_din=2;    //ccr reg_din=0;  reg_din=3; reg_din=5;  CHANGE CCR
+
+$display(" start pulse applied ");
+
+@(negedge clk) nwr=1; start=1;   nrd=1;             // start pulse given at negedge of clock
+@(negedge clk )  start=0;                  // start pulse duration is one clock cycle
+
+
+
+
+//~~~~~~~~~~~~~~~~~ #1 PLR<ULR<LLR ~~~~~~~~~~~~~~~~~~~~~~~~//
+
+
+nwr=0; reset=1; ncs=0; nrd=1;
+@(negedge clk) a0=0; a1=0; reg_din=5; // plr
+@(negedge clk) a0=1; a1=0; reg_din=10;    // ulr
+@(negedge clk) a0=0; a1=1; reg_din=15;    // llr
+@(negedge clk) a0=1; a1=1; reg_din=2;    //ccr reg_din=0;  reg_din=3; reg_din=5;  CHANGE CCR
+
+$display(" start pulse applied ");
+
+@(negedge clk) nwr=1; start=1;   nrd=1;             // start pulse given at negedge of clock
+@(negedge clk )  start=0;                  // start pulse duration is one clock cycle
+
+
+
+//~~~~~~~~~~~~~~~~~ #2 PLR<ULR>LLR ~~~~~~~~~~~~~~~~~~~~~~~~//
+
+
+
+nwr=0; reset=1; ncs=0; nrd=1;
+@(negedge clk) a0=0; a1=0; reg_din=5; // plr
+@(negedge clk) a0=1; a1=0; reg_din=10;    // ulr
+@(negedge clk) a0=0; a1=1; reg_din=2;    // llr
+@(negedge clk) a0=1; a1=1; reg_din=2;    //ccr reg_din=0;  reg_din=3; reg_din=5;  CHANGE CCR
+
+$display(" start pulse applied ");
+
+@(negedge clk) nwr=1; start=1;   nrd=1;             // start pulse given at negedge of clock
+@(negedge clk )  start=0;                  // start pulse duration is one clock cycle
+
+
+//~~~~~~~~~~~~~~~~~ #3 PLR>ULR<LLR ~~~~~~~~~~~~~~~~~~~~~~~~//
+
+
+nwr=0; reset=1; ncs=0; nrd=1;
+@(negedge clk) a0=0; a1=0; reg_din=15; // plr
+@(negedge clk) a0=1; a1=0; reg_din=10;    // ulr
+@(negedge clk) a0=0; a1=1; reg_din=20;    // llr
+@(negedge clk) a0=1; a1=1; reg_din=2;    //ccr reg_din=0;  reg_din=3; reg_din=5;  CHANGE CCR
+
+$display(" start pulse applied ");
+
+@(negedge clk) nwr=1; start=1;   nrd=1;             // start pulse given at negedge of clock
+@(negedge clk )  start=0;                  // start pulse duration is one clock cycle
+
+
+//~~~~~~~~~~~~~~~~~ #4 PLR>ULR>LLR ~~~~~~~~~~~~~~~~~~~~~~~~//
+
+
+nwr=0; reset=1; ncs=0; nrd=1;
+@(negedge clk) a0=0; a1=0; reg_din=15; // plr
+@(negedge clk) a0=1; a1=0; reg_din=10;    // ulr
+@(negedge clk) a0=0; a1=1; reg_din=5;    // llr
+@(negedge clk) a0=1; a1=1; reg_din=2;    //ccr reg_din=0;  reg_din=3; reg_din=5;  CHANGE CCR
+
+$display(" start pulse applied ");
+
+@(negedge clk) nwr=1; start=1;   nrd=1;             // start pulse given at negedge of clock
+@(negedge clk )  start=0;                  // start pulse duration is one clock cycle
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~ #8 MIDDLE START ~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+
+nwr=0; reset=1; ncs=0; nrd=1;
+@(negedge clk) a0=0; a1=0; reg_din=10; // plr
+@(negedge clk) a0=1; a1=0; reg_din=15;    // ulr
+@(negedge clk) a0=0; a1=1; reg_din=5;    // llr
+@(negedge clk) a0=1; a1=1; reg_din=2;    //ccr reg_din=0;  reg_din=3; reg_din=5;  CHANGE CCR
+
+$display(" start pulse applied ");
+
+@(negedge clk) nwr=1; start=1;   nrd=1;             // start pulse given at negedge of clock
+@(negedge clk )  start=0;                  // start pulse duration is one clock cycle
+
+@(negedge clk)   // counting happening here
+@(negedge clk)
+@(negedge clk)
+@(negedge clk)
+@(negedge clk)
+@(negedge clk)
+@(negedge clk)
+@(negedge clk)
+
+@(negedge clk) nwr=1; start=1;   nrd=1;             // start pulse given at negedge of clock
+@(negedge clk )  start=0;                  // start pulse duration is one clock cycle
+
+
+//~~~~~~~~~~~~~~~~~~~~~#9 MIDDLE NCS ~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+nwr=0; reset=1; ncs=0; nrd=1;
+@(negedge clk) a0=0; a1=0; reg_din=10; // plr
+@(negedge clk) a0=1; a1=0; reg_din=15;    // ulr
+@(negedge clk) a0=0; a1=1; reg_din=5;    // llr
+@(negedge clk) a0=1; a1=1; reg_din=2;    //ccr reg_din=0;  reg_din=3; reg_din=5;  CHANGE CCR
+
+@(negedge clk) nwr=1; start=1;   nrd=1;             // start pulse given at negedge of clock
+@(negedge clk )  start=0;                  // start pulse duration is one clock cycle
+
+@(negedge clk)   // counting happening here
+@(negedge clk)
+@(negedge clk)
+@(negedge clk)
+@(negedge clk)
+@(negedge clk)
+@(negedge clk)
+@(negedge clk)
+
+@(negedge clk) ncs=1; nwr=1; start=1;   nrd=1;            
+
+
+
+
+
+@(negedge clk) ncs=1; nwr=0; start=1;   nrd=1;            
+
+//~~~~~~~~~~~~~~~~~~~~~#10 MIDDLE RESET ~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+nwr=0; reset=1; ncs=0; nrd=1;
+@(negedge clk) a0=0; a1=0; reg_din=5; // plr
+@(negedge clk) a0=1; a1=0; reg_din=5;    // ulr
+@(negedge clk) a0=0; a1=1; reg_din=5;    // llr
+@(negedge clk) a0=1; a1=1; reg_din=2;    //ccr reg_din=0;  reg_din=3; reg_din=5;  CHANGE CCR
+
+@(negedge clk) nwr=1; start=1;   nrd=1;             // start pulse given at negedge of clock
+@(negedge clk )  start=0;                  // start pulse duration is one clock cycle
+
+@(negedge clk)   // counting happening here
+@(negedge clk)
+@(negedge clk)
+@(negedge clk)
+@(negedge clk)
+@(negedge clk)
+@(negedge clk)
+@(negedge clk)
+
+//~~~~~~~~~~~~~~~~~~~~~#11 MIDDLE NWR ~~~~~~~~~~~~~~~~~~~~~~~~~//
+nwr=0; reset=1; ncs=0; nrd=1;
+@(negedge clk) a0=0; a1=0; reg_din=5; // plr
+@(negedge clk) a0=1; a1=0; reg_din=5;    // ulr
+@(negedge clk) a0=0; a1=1; reg_din=5;    // llr
+@(negedge clk) a0=1; a1=1; reg_din=2;    //ccr reg_din=0;  reg_din=3; reg_din=5;  CHANGE CCR
+
+@(negedge clk) nwr=1; start=1;   nrd=1;             // start pulse given at negedge of clock
+@(negedge clk )  start=0;                  // start pulse duration is one clock cycle
+
+@(negedge clk)   // counting happening here
+@(negedge clk)
+@(negedge clk)
+@(negedge clk)
+@(negedge clk)
+@(negedge clk)
+@(negedge clk)
+@(negedge clk)
+
+@(negedge clk) ncs=1; nwr=0; start=1;   nrd=1;   {a1,a0}=0; reg_din=20;         //plr
+
+@(negedge clk) ncs=1; nwr=0; start=1;   nrd=1;   {a1,a0}=1; reg_din=30;         // ulr
+@(negedge clk) ncs=1; nwr=0; start=1;   nrd=1;   {a1,a0}=2; reg_din=15;         // llr
+@(negedge clk) ncs=1; nwr=0; start=1;   nrd=1;   {a1,a0}=3; reg_din=2;         //ccr
+
+
+//~~~~~~~~~~~~~~~~~~~~~#12 MIDDLE NRD ~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+nwr=0; reset=1; ncs=0; nrd=1;
+@(negedge clk) a0=0; a1=0; reg_din=5; // plr
+@(negedge clk) a0=1; a1=0; reg_din=5;    // ulr
+@(negedge clk) a0=0; a1=1; reg_din=5;    // llr
+@(negedge clk) a0=1; a1=1; reg_din=2;    //ccr reg_din=0;  reg_din=3; reg_din=5;  CHANGE CCR
+
+@(negedge clk) nwr=1; start=1;   nrd=1;             // start pulse given at negedge of clock
+@(negedge clk )  start=0;                  // start pulse duration is one clock cycle
+
+@(negedge clk)   // counting happening here
+@(negedge clk)
+@(negedge clk)
+@(negedge clk)
+@(negedge clk)
+@(negedge clk)
+@(negedge clk)
+@(negedge clk)
+@(negedge clk) ncs=1; nwr=1; start=1;   nrd=0;   {a1,a0}=0;
+@(negedge clk) ncs=1; nwr=1; start=1;   nrd=0;   {a1,a0}=1;
+@(negedge clk) ncs=1; nwr=1; start=1;   nrd=0;   {a1,a0}=2;
+@(negedge clk) ncs=1; nwr=1; start=1;   nrd=0;   {a1,a0}=3;
+
+
+
+
+
+
+
+
+
+end
+endtask
+// ========== Debug output========================================//
+task debug_output;
+	 begin
+ 		$display("----------------------------------------------");
+        $display("------------------     -----------------------");
+ 		$display("----------- SIMULATION RESULT ----------------");
+	    $display("--------------             -------------------");
+		$display("----------------         ---------------------");
+		$display("----------------------------------------------");
+$monitor("TIME = %0d,clk=%b reset = %b, ncs = %b, nwr=%b reg_din=%0d  nrd = %b, start = %d ao=%b a1=%b err=%b ec=%b dir=%b count=%0d",$time,clk,reset,ncs,nwr,reg_din,nrd,start,a0,a1,err,ec,dir,count);
+	 end
+endtask
+
+//================END SIMULATION ==================================//
+task endsimulation;
+ begin
+#500
+ $display("-------------- THE SIMUALTION END ------------");
+ $finish;
+ end
+endtask
+    
+endmodule
---
+++ Sequential_Circuits/project_1_up_down_counter/tester.v
@@ -0,0 +1,326 @@
+// Code your design here
+module up_down_counter255_tester (inout [7:0]Din,input clk_in,ncs_in,nrd_in,nwr_in,start_in,reset_in,A0,A1 ,output reg [7:0]count_out,output reg err_out,ec_out,dir_out);
+reg start_flag; // start flag is raised whenever start pulse comes 
+reg [7:0]reg_out;  // storing din out value in reg
+// for storing input 
+
+
+reg [7:0]temp2;
+
+integer pulse_counter=0;
+
+assign Din=(ncs_in==0 && nrd_in==0 && nwr_in==1)? reg_out:8'bz;  // whenever value in din out changes the it iis assigned to din 
+//  inout port must be wire so we are unable to use in procedural blocks(while
+//  reading) taken a register as reg_out values will be taken to it{while
+//  reading}| and
+//  assigned to wire Din	using assign keyword***************///
+
+reg [7:0]PLR,ULR,CCR,LLR;
+// registers for storing values required for counter
+// ===>PLR = stores  count start value 
+// ======>ULR = stores upperlimit of count
+// ========>LLR = stores lower limit of count
+//==>CCR= number of times to count
+// reg [9:0]temp,no_of_PLRs;
+// temp is used to calculate the no of PLRs came in counting sequence
+
+reg up_flag,down_flag,preload_flag;
+// these flags will control flow of count
+
+always@(posedge clk_in)
+		begin //{
+			if(ncs_in==0)
+					begin //{ chip select on
+						if(reset_in==0)
+										begin //{   // reset==0
+											PLR=0;
+											LLR=0;
+											CCR=0;
+											ULR=8'd255;  
+											ec_out=1'bz;
+											err_out=1'bz;
+                                            dir_out=1'bz;    /// doubt  
+                                            start_flag=1'bz; 
+											pulse_counter=0;
+                                            temp2=8'bz;
+											count_out=8'bz;
+
+						   				 end //} reset==0 end
+						else  // reset ==1 case
+											begin //{
+																	if(nwr_in==0)
+																		begin //{ // writing start
+                                                                      //   $display("data is writing plr=%0d ulr=%0d llr=%0d ccr=%0d",PLR,ULR,LLR,CCR);
+																	//	err_out=0;
+																		case({A1,A0})
+                                  					     				2'b00: begin PLR=Din; end
+					                                       				2'b01: begin ULR=Din;end
+                    							                   		2'b10: begin LLR=Din;end
+                    		                   							2'b11: begin CCR=Din;end
+																		 default : begin   		PLR=PLR; LLR=LLR; CCR=CCR; ULR=ULR;   		 end
+																		endcase
+																	end //} writing end
+															else if(nrd_in==0)
+																	begin//{
+																		case({A1,A0})
+        		                               							2'b00: begin reg_out=PLR;end
+                		                       							2'b01: begin reg_out=ULR;end
+                                		       							2'b10: begin reg_out=LLR;end
+                        		               							2'b11: begin reg_out=CCR;end
+																		default : begin   		reg_out=8'bz;   		 end
+																		endcase
+																	end//}
+															else
+																begin  //{
+                                                                    					count_out=count_out;
+    
+												  				  end//} */
+											
+															end//}
+															
+				end//} ncs =0 end
+				else
+					begin//{
+					count_out=count_out;
+                    PLR=PLR;
+                    LLR=LLR;
+                    CCR=CCR;
+                    ULR=ULR;
+                    start_flag=start_flag;
+                    temp2=temp2;
+                    err_out=err_out;
+                    dir_out=dir_out;
+                    ec_out=ec_out;
+                    count_out=count_out;
+					end//}
+	   end//} always end
+//===========================================ERROR SIGNAL GENERATION========================//
+always@(posedge clk_in)
+		begin//{
+		if(PLR<LLR || PLR>ULR)
+			begin//{
+				err_out=1;
+			end//}
+			else
+				begin//{
+				err_out=0;
+                 
+				end//}
+		end//}
+	
+			
+//====================== START FLAG ==============================//
+always@(negedge start_in)
+		begin//{
+		if(pulse_counter==1 && ncs_in==0 && reset_in==1 && nwr_in!==0 && nrd_in!==0 && err_out==0) // starts only when one pulse is obtained and ncs=0 reset =1 ,.....
+		begin//{
+				if(CCR==0)         // storing CCR values to temporary register
+					begin				
+					ec_out=1; 
+					pulse_counter=0;
+					end
+					else if(CCR>0)
+					begin
+					temp2=CCR; ec_out=0; 
+				start_flag=1;
+				//dir_out=1;         // default direction is up ???? doubt 
+				up_flag=1;         // default direction is up counting
+				count_out=PLR-1;   // loaded with plr-1 but show count from PLR
+					end
+                    else
+                    begin
+                        ec_out=1;
+                        pulse_counter=0;
+                        start_flag=0;
+                    end
+		end//}
+		else 
+						begin//{    
+							start_flag=0;  // pulse count>1 || ncs=1 || reset=0 || nwr=0 || nrd=0 || err=1 endcycle=0    
+						end//}
+
+		end//}
+always@(posedge clk_in)
+		begin//{
+		 if(start_in==1 && ncs_in==0 && nwr_in!==0 && reset_in==1 && nrd_in!==0 && err_out==0)
+				begin//{
+					pulse_counter=pulse_counter+1;   // counting start pulses
+				end//}
+				else
+					begin//{
+						pulse_counter=pulse_counter;   
+	
+					end//}
+		end//}
+///==================================================================//
+/* always@(negedge clk_in)
+		begin//{
+				if(temp2==0 && ncs_in==0 && nwr_in!==0 && reset_in==1 && nrd_in!==0 && err_out==0 && ec_out==0)
+					begin
+					//	ec_out=1;
+						start_flag=0;
+						count_out=count_out;
+						temp2=8'bz;
+				end
+				else
+					begin
+						ec_out=0;
+					end
+		end//}*/
+
+// ===========================COUNTER BLOCK====================//
+//----------------UP COUNTING 1------------------------------//
+always @(posedge clk_in )
+		begin//{
+			if(start_flag==1 && ncs_in==0 && nwr_in!==0 && reset_in==1 && nrd_in!==0 && err_out==0 && ec_out==0)
+				begin//{
+					if(up_flag==1)
+							begin//{
+								if(count_out<ULR) 
+									begin//{
+										count_out=count_out+1; dir_out=1;   
+									end//}	
+								else // count ==ulr
+									begin//{
+										if(count_out==PLR && count_out==ULR && count_out==LLR )
+											begin//{
+											 temp2=temp2-1;
+											if(temp2==1) 
+                                                begin
+											        ec_out=1;
+													pulse_counter=0;
+
+											start_flag=0;
+											count_out=count_out;
+											temp2=8'bz;
+                                            	end
+                                                else
+                                                begin
+                                                    ec_out=ec_out;
+                                                end
+											end//}
+											else
+                                                 begin
+											        down_flag=1; up_flag=0; 
+                                                end
+					    				end//}
+end//}
+					else
+							begin//{
+									count_out=count_out; dir_out=dir_out;
+
+								end//}
+end//}
+else
+begin
+		count_out=count_out; dir_out=dir_out;
+end
+end//}
+
+//============================DOWN COUNTING================================//
+always@(posedge clk_in)		
+		begin//{
+if(start_flag==1 && ncs_in==0 && nwr_in!==0 && reset_in==1 && nrd_in!==0 && err_out==0 && ec_out==0)		begin//{
+				 	 if(down_flag==1)
+							begin//{
+								 if(count_out>LLR)
+								begin//{
+									count_out=count_out-1; dir_out=0;// end
+
+									if((count_out==PLR && count_out==LLR) && temp2==1)	
+									    begin
+										    ec_out=1;
+											pulse_counter=0;
+
+						start_flag=0;
+						count_out=count_out;
+						temp2=8'bz;
+										end
+
+									end//}	
+								else 
+								begin//{
+									preload_flag=1; down_flag=0; 
+								end//}
+	
+							
+						end//}
+
+				    	
+
+					else
+							begin//{
+									count_out=count_out; dir_out=dir_out;
+
+							end//}
+							end//}
+
+else
+begin
+		count_out=count_out; dir_out=dir_out;
+end
+
+end//}
+
+//==============================END CYCLE============================//
+	always@(posedge clk_in)
+	begin	
+		if(ec_out==1) begin
+				ec_out<=0; end
+				else begin
+				ec_out<=ec_out;
+                end
+end
+// ===============================UP COUNTING UPTO PLR===================//
+always@(posedge clk_in)
+begin//{
+if(start_flag==1 && ncs_in==0 && nwr_in!==0 && reset_in==1 && nrd_in!==0 && err_out==0 && ec_out==0)		begin//{
+					 if(preload_flag==1)
+							begin//{
+								if(count_out<PLR)
+									begin//{
+										count_out=count_out+1; dir_out=1;
+										if(count_out==PLR && temp2==1)
+											begin
+												ec_out=1;
+												pulse_counter=0;
+
+						start_flag=0;
+						count_out=count_out;
+						temp2=8'bz;
+											end
+                                            else
+                                            begin
+                                                ec_out=ec_out;
+                                            end
+									end//}
+									else 
+									begin//{	
+										temp2=temp2-1; up_flag=1; preload_flag=0;
+										
+										
+									end//}
+							
+								end//}		
+					else
+							begin//{
+									count_out=count_out; dir_out=dir_out;
+
+							end//}
+							end//}
+
+					else
+						begin
+						count_out=count_out; dir_out=dir_out;
+						end
+
+					
+					
+		end//}
+///////////======================== THE END  =======================//
+
+
+
+
+
+    endmodule
